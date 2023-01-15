15ca1e63b7dfb7f436cebaf7b2e7cc9760e7f92c
Chris Troutner <chris.troutner@gmail.com>
2023-01-15 09:55:11 -0800

    fixing linting errors

--- public/electron.js
+++ public/electron.js
@@ -3,7 +3,7 @@ const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 
 const path = require('path')
-const url = require('url')
+// const url = require('url')
 const isDev = require('electron-is-dev')
 
 let mainWindow
@@ -11,7 +11,7 @@ let mainWindow
 function createWindow () {
   mainWindow = new BrowserWindow({ width: 900, height: 680 })
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`)
-  mainWindow.on('closed', () => mainWindow = null)
+  mainWindow.on('closed', () => { mainWindow = null })
 }
 
 app.on('ready', createWindow)
